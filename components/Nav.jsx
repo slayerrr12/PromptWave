b435dcbe06182fe2b64ab6242403fa63424cfb62
slayerrr12 <eyeamarinsharma@gmail.com>
2023-06-27 00:05:55 +0530

    created the navbar componenet for desktop view

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,6 +12,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 
 const Nav = () => {
+    const isUserLoggedIn = true;
     return (
         <nav className='flex=between w-full mb-16 pt-3'>
             <Link href='/' className='flex gap-2 flex-right'>
@@ -22,12 +23,40 @@ const Nav = () => {
 
                     <Typewriter
                         options={{
-                            strings: ['PromptWave','Find Awesome Fonts'],
+                            strings: ['PromptWave', 'Find Awesome Fonts'],
                             autoStart: true,
                             loop: true,
                         }}
                     /></p>
             </Link>
+
+            <div className='sm:flex hidden'>
+                {isUserLoggedIn ? (
+                    <div className='flex gap-3 md:gap-5'>
+                        <Link href='/create-prompt' className='"black_btn'>
+                            Create Post
+
+                        </Link>
+                        <button type="button" onClick={signOut} className='outline_btn'>
+                            Sign Out
+
+                        </button>
+                        <Link href="/profile">
+                            <Image src="/assets/images/profile.svg" width={37} height={37} className='' />
+                        </Link>
+
+                    </div>
+
+                ) : (<>
+
+
+
+
+
+
+                </>)}
+
+            </div>
 
 
 
