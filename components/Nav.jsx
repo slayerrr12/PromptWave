5a67d34fbaac93a35671e9116cdc4c99e4430fa7
slayerrr12 <eyeamarinsharma@gmail.com>
2023-06-30 21:11:17 +0530

    issue fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,108 +8,102 @@ import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 
 const Nav = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const [providers, setProviders] = useState(null);
+    const router = useRouter();
+    const { data: session } = useSession();
+    const [providers, setProviders] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const res = await getProviders();
-      setProviders(res);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const res = await getProviders();
+            setProviders(res);
+        })();
+    }, []);
+    const handleSignOut = async () => {
+        await signOut();
+        router.push("/");
+    };
 
-  useEffect(() => {
-    if (!session?.user) {
-      router.push("/");
-    }
-  }, [session]);
+    return (
+        <nav className="min-w-full flex justify-between items-center py-4 px-6">
+            <div className="flex-initial">
+                <Link href="/" className="flex items-center gap-x-5">
+                    <Image
+                        src="/assets/images/logo.svg"
+                        alt="logo"
+                        width={30}
+                        height={30}
+                        className="object-contain"
+                    />
+                    <p className="logo_text">
+                        <Typewriter
+                            options={{
+                                strings: ["PromptWave", "Find Awesome Prompts"],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </p>
+                </Link>
+            </div>
 
-  return (
-    <nav className="min-w-full flex justify-between items-center py-4 px-6">
-      <div className="flex-initial">
-        <Link href="/" className="flex items-center gap-x-5">
-          <Image
-            src="/assets/images/logo.svg"
-            alt="logo"
-            width={30}
-            height={30}
-            className="object-contain"
-          />
-          <p className="logo_text">
-            <Typewriter
-              options={{
-                strings: ["PromptWave", "Find Awesome Prompts"],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </p>
-        </Link>
-      </div>
+            <div className="flex-auto"></div>
 
-      <div className="flex-auto"></div>
+            <div className="flex-initial">
+                <div className="flex gap-3 md:gap-5 items-center">
+                    {session?.user ? (
+                        <>
+                            <Link href="/createpost">
+                                <button class="relative inline-block px-4 py-2 font-medium group">
+                                    <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                    <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                    <span class="relative text-black group-hover:text-white">
+                                        Create Post
+                                    </span>
+                                </button>
+                            </Link>
 
-      <div className="flex-initial">
-        <div className="flex gap-3 md:gap-5 items-center">
-          {session?.user ? (
-            <>
-              <Link href="/createpost">
-                <button
-                  class="relative inline-block px-4 py-2 font-medium group"
-                >
-                  <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                  <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                  <span class="relative text-black group-hover:text-white">
-                    Create Post
-                  </span>
-                </button>
-              </Link>
+                            <a
+                                onClick={handleSignOut}
+                                class="relative inline-block px-4 py-2 font-medium group"
+                            >
+                                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                <span class="relative text-black group-hover:text-white">
+                                    Sign Out
+                                </span>
+                            </a>
 
-              <a
-                onClick={() => {
-                  signOut();
-                }}
-                class="relative inline-block px-4 py-2 font-medium group"
-              >
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">
-                  Sign Out
-                </span>
-              </a>
-
-              <Link href="/profile">
-                <Image
-                  src={session?.user.image}
-                  width={37}
-                  height={37}
-                  className="rounded-full"
-                  alt="profile"
-                />
-              </Link>
-            </>
-          ) : (
-            <>
-              {providers &&
-                Object.values(providers).map((provider) => (
-                  <button
-                    type="button"
-                    key={provider.name}
-                    onClick={() => {
-                      signIn(provider.id);
-                    }}
-                    className="black_btn"
-                  >
-                    Sign in
-                  </button>
-                ))}
-            </>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+                            <Link href="/profile">
+                                <Image
+                                    src={session?.user.image}
+                                    width={37}
+                                    height={37}
+                                    className="rounded-full"
+                                    alt="profile"
+                                />
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            {providers &&
+                                Object.values(providers).map((provider) => (
+                                    <button
+                                        type="button"
+                                        key={provider.name}
+                                        onClick={() => {
+                                            signIn(provider.id);
+                                        }}
+                                        className="black_btn"
+                                    >
+                                        Sign in
+                                    </button>
+                                ))}
+                        </>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default dynamic(() => Promise.resolve(Nav), { ssr: false });
