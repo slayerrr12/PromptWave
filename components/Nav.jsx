fa09ea4f94bf71f82b5a7d6472d1a5d5d346339a
slayerrr12 <eyeamarinsharma@gmail.com>
2023-06-30 21:27:24 +0530

    issue Fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,28 +1,28 @@
-'use client'
+"use client";
+
 import Typewriter from "typewriter-effect";
-import React, { useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-import { useRouter } from "next/router";
+import { usePathname, useRouter } from "next/navigation";
+import { redirect } from 'next/navigation'
 import dynamic from "next/dynamic";
 
 const Nav = () => {
-    
+    const router = useRouter();
+
     const { data: session } = useSession();
     const [providers, setProviders] = useState(null);
-
     useEffect(() => {
         (async () => {
-            const res = await getProviders();
-            setProviders(res);
+          const res = await getProviders();
+          setProviders(res);
         })();
-    }, []);
-    
-
+      }, []);
     return (
-        <nav className="min-w-full flex justify-between items-center py-4 px-6">
+        <nav className=" min-w-full flex justify-between items-center py-4 px-6">
             <div className="flex-initial">
                 <Link href="/" className="flex items-center gap-x-5">
                     <Image
@@ -50,13 +50,33 @@ const Nav = () => {
                 <div className="flex gap-3 md:gap-5 items-center">
                     {session?.user ? (
                         <>
-                            <Link href='/create-prompt' className='black_btn'>
-                                Create Post
+                            <Link href="/createpost">
+                                <button
+                                    
+                                
+                                    
+                                    class="relative inline-block px-4 py-2 font-medium group"
+                                >
+                                    <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                    <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                    <span class="relative text-black group-hover:text-white">
+                                        Create Post
+                                    </span>
+                                </button>
                             </Link>
 
-                            <button type='button' onClick={signOut} className='outline_btn'>
-                                Sign Out
-                            </button>
+                            <a
+                                
+                                
+                                onClick={signOut}
+                                class="relative inline-block px-4 py-2 font-medium group"
+                            >
+                                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                <span class="relative text-black group-hover:text-white">
+                                    Sign Out
+                                </span>
+                            </a>
 
                             <Link href="/profile">
                                 <Image
@@ -83,6 +103,7 @@ const Nav = () => {
                                         Sign in
                                     </button>
                                 ))}
+
                         </>
                     )}
                 </div>
