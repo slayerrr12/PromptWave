3262415d8627367e28f8395f215ce0a2dd1ef53c
slayerrr12 <eyeamarinsharma@gmail.com>
2023-06-30 20:47:37 +0530

    signout issue fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -68,10 +68,7 @@ const Nav = () => {
                             <a
                                 href="/"
                                 on
-                                onClick={()=>{
-                                    redirect('/')
-                                    signOut()
-                                }}
+                                onClick={signOut}
                                 class="relative inline-block px-4 py-2 font-medium group"
                             >
                                 <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
