eb0cd634d81e99a4e0eaea4f00372173137cbe3e
Arin Sharma <111631186+slayerrr12@users.noreply.github.com>
2024-03-26 17:00:02 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "PromptWave",
+    title: "PromptoPedia",
     description: "Discover & Share AI Prompts",
 };
 
